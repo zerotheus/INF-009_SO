b78cdd9a4098a8b9fcbef8a6ff1729e7e2b44bc2
Ntheus <matheusoliveira.7313@gmail.com>
2023-04-05 17:02:31 -0300

    mutex

--- threadsImplementacao.cpp
+++ threadsImplementacao.cpp
@@ -5,25 +5,48 @@
 using namespace std;
 
 int x = 2;
+int mutex = 0;
 
-void task1(std::string msg)
+void mutexDown()
 {
-    x = x + 1;
-    printf("thread soma: %d\n", x);
+    mutex = 1;
 }
 
-void task2(std::string msg)
+void mutexUp()
 {
+    mutex = 0;
+}
+
+void task1()
+{
+    while (mutex != 0)
+    {
+        printf("espera task1\n");
+    }
+    mutexDown();
+    x = x + 1;
+    printf("thread soma: %d\n", x);
+    mutexUp();
+}
+
+void task2()
+{
+    while (mutex != 0)
+    {
+        printf("espera task2\n");
+    }
+    mutexDown();
     x = x * 2;
     printf("thread multi: %d\n", x);
+    mutexUp();
 }
 
 int main()
 {
-    thread t1(task1, "hello thread\n");
-    thread t2(task2, "hello thread\n");
-    thread t3(task2, "hello thread\n");
-    thread t4(task1, "hello thread\n");
+    thread t1(task1);
+    thread t2(task2);
+    thread t3(task2);
+    thread t4(task1);
 
     t3.join();
     t2.join();
