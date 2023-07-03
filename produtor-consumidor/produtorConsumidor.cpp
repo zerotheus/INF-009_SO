4c1674e46162db8c09365f6ae66e7d097556cc78
ntheus <matheusoliveira.7313@gmail.com>
2023-07-03 04:00:12 -0300

    esbarrado em problema de sleep wakeup c++

--- produtor-consumidor/produtorConsumidor.cpp
+++ produtor-consumidor/produtorConsumidor.cpp
@@ -1,9 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <thread>
+#include <mutex>
+#include <atomic>
+using namespace std;
 
 int buffer[100]; // RC
 int cheio = 0;
 int vazio = 99;
+mutex mutual;
+atomic<bool> mutualValue(false);
+
+void produtor();
+void produzir(int pos);
+void consumidor();
+void consumir(int pos);
+void espera();
+void ativaMutex();
+void desativaMutex();
+
+thread produtora(produtor);
 
 int main()
 {
@@ -13,6 +29,8 @@ int main()
         buffer[i] = 0;
     }
     printf("De volta");
+    // thread consumidora(consumidor);
+    produtora.join();
 }
 
 void produtor()
@@ -21,18 +39,20 @@ void produtor()
     if (cheio == 99)
     {
         printf("produtor sleep");
-        // sleep
     }
-    produzir(); // RC
+    espera();
+    ativaMutex();
+    produzir(produzEm); // RC
+    desativaMutex();
 }
 
-void produzir()
+void produzir(int pos)
 {
     vazio--;
     cheio++;
 }
 
-void consumidor()
+void consumidor(int pos)
 {
     static int consomeEm = 0;
     if (vazio == 0)
@@ -40,11 +60,30 @@ void consumidor()
         printf("consumidor sleep");
         // sleep
     }
-    consumir(); // RC
+    consumir(consomeEm); // RC
 }
 
-void consumir()
+void consumir(int pos)
 {
     vazio++;
     cheio--;
+}
+
+void espera()
+{
+    while (mutualValue.load())
+    {
+    }
+}
+
+void ativaMutex()
+{
+    mutual.lock();
+    mutualValue.store(true);
+}
+
+void desativaMutex()
+{
+    mutual.unlock();
+    mutualValue.store(false);
 }
